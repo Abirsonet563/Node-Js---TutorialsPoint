e437fe6ccdc54a4109e1deeb8c0c134b294908ab
Abir Hasan <abirsonet563@gmail.com>
2024-12-26 23:36:04 +0000

    WriteFile

--- file system module/index.js
+++ file system module/index.js
@@ -1,25 +1,6 @@
-const fs = require('fs'); // File System
-
- //Create a Directory
-// fs.mkdir('myFolder', (err) => {
-//     if(err) throw err;
-//     console.log('File Created')
-// })
-
-  //Creating Subdirectories
-// fs.mkdir('parent/child', {recursive: true}, (err) => {
-//     if(err) throw err;
-//     console.log('Subdirectories Created!')
-// })
-
-//Removed Directories
-// fs.rmdir('./myFolder', (err) => {
-//     if(err) throw err;
-//     console.log('Removed Directory')
-// })
-
-//Read a File
-fs.readFile('./tutorialspoint.txt','utf-8', (err, data) => {
-  if(err) throw err;
-  console.log(data);
+const fs = require('fs');
+//Write File
+fs.writeFile('greeting.txt', 'Welcome to Node |JS Part-2', (err) =>{
+    if(err) throw err;
+    console.log('Operation Successful');
 })
