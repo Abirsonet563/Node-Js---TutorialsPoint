a0eeec38c484d7eb42aac19a2da5a05ac42c57b9
Abir Hasan <abirsonet563@gmail.com>
2024-12-26 22:40:42 +0000

    Created Subdirectories

--- file system module/index.js
+++ file system module/index.js
@@ -1,7 +1,12 @@
 const fs = require('fs'); // File System
 
-//Create a Directory
-fs.mkdir('myFolder', (err) => {
+// //Create a Directory
+// fs.mkdir('myFolder', (err) => {
+//     if(err) throw err;
+//     console.log('File Created')
+// })
+
+fs.mkdir('parent/child', {recursive: true}, (err) => {
     if(err) throw err;
-    console.log('File Created')
+    console.log('Subdirectories Created!')
 })
