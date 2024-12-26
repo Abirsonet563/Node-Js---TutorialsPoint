b4b2d44ed2682c048d867f049380fb0ca6bf266a
Abir Hasan <abirsonet563@gmail.com>
2024-12-26 22:51:03 +0000

    Remove Directories

--- file system module/index.js
+++ file system module/index.js
@@ -1,12 +1,19 @@
 const fs = require('fs'); // File System
 
-// //Create a Directory
+ //Create a Directory
 // fs.mkdir('myFolder', (err) => {
 //     if(err) throw err;
 //     console.log('File Created')
 // })
 
-fs.mkdir('parent/child', {recursive: true}, (err) => {
+  //Creating Subdirectories
+// fs.mkdir('parent/child', {recursive: true}, (err) => {
+//     if(err) throw err;
+//     console.log('Subdirectories Created!')
+// })
+
+//Removed Directories
+fs.rmdir('./myFolder', (err) => {
     if(err) throw err;
-    console.log('Subdirectories Created!')
+    console.log('Removed Directory')
 })
