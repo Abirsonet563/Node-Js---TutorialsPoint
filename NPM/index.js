e30ef2bd8e8e4b62b0c9eb63ce0c975edbdc4495
Abir Hasan <abirsonet563@gmail.com>
2024-12-27 01:42:02 +0000

    Render HTML code on Server

--- NPM/index.js
+++ NPM/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) =>{
-    res.send('Welcome to Our Server')
+    // res.send('Welcome to Our Server')
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get('/contact', (req, res) =>{
+    res.send('<h1>Welcome to Contact Page</h1>')    
 })
 app.listen(3000, () => {
     console.log('Server Started On Port 3000');
